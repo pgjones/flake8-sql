f963202cf9666e4393805c7b4f89bca0717507dd
pgjones <philip.graham.jones@googlemail.com>
2017-02-02 20:41:53 +0000

    Wrap sqlparse aspects in more convienient classes
    This abstracts some of the sqlparse difficulties.

--- flake8_sql/linter.py
+++ flake8_sql/linter.py
@@ -2,10 +2,8 @@ import ast
 import re
 from typing import Any, Generator, Iterable, List, Optional, Tuple, TypeVar
 
-import sqlparse
-
-from .keywords import ABBREVIATED_KEYWORDS, KEYWORDS
-from .parser import parse
+from .keywords import ABBREVIATED_KEYWORDS
+from .parser import Parser, parse
 
 
 __version__ = '0.1'
@@ -30,60 +28,44 @@ class Linter:
     def run(self) -> Generator[Tuple[int, int, str, type], Any, None]:
         for node in ast.walk(self.tree):
             if isinstance(node, ast.Str) and SQL_RE.search(node.s) is not None:
-                yield from self._check_query_words(node)
-                yield from self._check_query_whitespace(node)
-                yield from self._check_query_linespace(node)
+                parser = Parser(node.s)
+                yield from self._check_query_words(node, parser)
+                yield from self._check_query_whitespace(node, parser)
+                yield from self._check_query_linespace(node, parser)
 
     def _check_query_words(
-            self, query: ast.Str,
+            self, query: ast.Str, parser: Parser,
     ) -> Generator[Tuple[int, int, str, type], Any, None]:
-        statements = sqlparse.parse(query.s)
-        for token in _flattened_statements(statements):
+        for token in parser:
             word = token.value
             if token.is_keyword:
-                yield from self._check_keyword(word, query)
-            elif token.ttype == sqlparse.tokens.Name:
-                # Function identifiers/names are not recognised as keywords in
-                # sqlparse, so this check is required. Note the only name-token
-                # who's grandparent is a function is the function identifier.
-                if (
-                        token.within(sqlparse.sql.Function) and
-                        isinstance(token.parent.parent, sqlparse.sql.Function) and
-                        sqlparse.keywords.is_keyword(word)[0] == sqlparse.tokens.Token.Keyword
-                ):
-                    yield from self._check_keyword(word, query)
-                elif not word.islower():
+                if not word.isupper():
                     yield(
                         query.lineno, query.col_offset,
-                        "Q441 name {} is not snake_case".format(word),
+                        "Q440 keyword {} is not uppercase".format(word),
                         type(self),
                     )
-
-    def _check_keyword(
-            self, word: str, query: ast.Str,
-    ) -> Generator[Tuple[int, int, str, type], Any, None]:
-        if not word.isupper():
-            yield(
-                query.lineno, query.col_offset,
-                "Q440 keyword {} is not uppercase".format(word),
-                type(self),
-            )
-        if word.upper() in ABBREVIATED_KEYWORDS:
-            yield(
-                query.lineno, query.col_offset,
-                "Q442 avoid abbreviated keywords, {}".format(word),
-                type(self),
-            )
+                if word.upper() in ABBREVIATED_KEYWORDS:
+                    yield(
+                        query.lineno, query.col_offset,
+                        "Q442 avoid abbreviated keywords, {}".format(word),
+                        type(self),
+                    )
+            elif token.is_name and not word.islower():
+                yield(
+                    query.lineno, query.col_offset,
+                    "Q441 name {} is not snake_case".format(word),
+                    type(self),
+                )
 
     def _check_query_whitespace(
-            self, query: ast.Str,
+            self, query: ast.Str, parser: Parser,
     ) -> Generator[Tuple[int, int, str, type], Any, None]:
-        statements = sqlparse.parse(query.s)
-        for before, token, after in _pre_post_iter(_flattened_statements(statements)):
+        for before, token, after in _pre_post_iter(parser):
             pre_whitespace = (before is not None and before.is_whitespace)
             post_whitespace = (after is not None and after.is_whitespace)
-            post_newline = (after is None or after.ttype == sqlparse.tokens.Text.Whitespace.Newline)
-            if token.ttype == sqlparse.tokens.Punctuation:
+            post_newline = (after is None or after.is_newline)
+            if token.is_punctuation:
                 if token.value == ',' and not post_whitespace:
                     yield(
                         query.lineno, query.col_offset,
@@ -97,7 +79,7 @@ class Linter:
                         type(self),
                     )
             elif (
-                    token.ttype == sqlparse.tokens.Comparison
+                    token.is_comparison
                     and (not pre_whitespace or not post_whitespace)
             ):
                 yield(
@@ -107,7 +89,7 @@ class Linter:
                 )
 
     def _check_query_linespace(
-            self, query: ast.Str,
+            self, query: ast.Str, parser: Parser,
     ) -> Generator[Tuple[int, int, str, type], Any, None]:
         parsed = parse(query)
         if len(parsed) == 0 or parsed[0].line == parsed[-1].line:
@@ -120,15 +102,6 @@ class Linter:
                 )
                 yield (query.lineno, query.col_offset, message, type(self))
             previous = phrase
-
-
-
-def _flattened_statements(
-        statements: Tuple[sqlparse.sql.Statement],
-) -> Generator[sqlparse.sql.Token, Any, None]:
-    for statement in statements:
-        for token in statement.flatten():
-            yield token
 
 
 T = TypeVar('T')
--- flake8_sql/parser.py
+++ flake8_sql/parser.py
@@ -1,6 +1,9 @@
 import ast
 import re
-from typing import List, NamedTuple
+from typing import Any, Generator, List, NamedTuple
+
+import sqlparse
+
 
 Phrase = NamedTuple('Phrase', [('phrase', str), ('line', int)])
 
@@ -29,3 +32,59 @@ def parse(query: ast.Str) -> List[Phrase]:
         else:
             parsed.append(Phrase(phrase.group().strip(), line))
     return parsed
+
+
+class Token:
+
+    def __init__(self, token: sqlparse.sql.Token) -> None:
+        self._token = token
+
+    @property
+    def is_whitespace(self) -> bool:
+        return self._token.is_whitespace
+
+    @property
+    def is_keyword(self) -> bool:
+        simple_keyword = self._token.is_keyword
+        # Function identifiers/names are not recognised as keywords in
+        # sqlparse, so this check is required. Note the only name-token
+        # who's grandparent is a function is the function identifier.
+        function_keyword = (
+            self._token.ttype == sqlparse.tokens.Name and
+            self._token.within(sqlparse.sql.Function) and
+            isinstance(self._token.parent.parent, sqlparse.sql.Function) and
+            sqlparse.keywords.is_keyword(self._token.value)[0] == sqlparse.tokens.Token.Keyword
+        )
+        return simple_keyword or function_keyword
+
+    @property
+    def is_name(self) -> bool:
+        return self._token.ttype == sqlparse.tokens.Name and not self.is_keyword
+
+    @property
+    def is_punctuation(self) -> bool:
+        return self._token.ttype == sqlparse.tokens.Punctuation
+
+    @property
+    def is_comparison(self) -> bool:
+        return self._token.ttype == sqlparse.tokens.Comparison
+
+    @property
+    def is_newline(self) -> bool:
+        return self._token.ttype == sqlparse.tokens.Text.Whitespace.Newline
+
+    @property
+    def value(self) -> str:
+        return self._token.value
+
+
+class Parser:
+
+    def __init__(self, sql: str) -> None:
+        self.sql = sql
+
+    def __iter__(self) -> Generator[Token, Any, None]:
+        statements = sqlparse.parse(self.sql)
+        for statement in statements:
+            for token in statement.flatten():
+                yield Token(token)
