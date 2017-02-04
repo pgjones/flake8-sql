93bc28b05922152ec1d4470293b9a09e61647ea9
pgjones <philip.graham.jones@googlemail.com>
2017-02-04 18:52:27 +0000

    Add test case for AND and OR alignment

--- tests/test_cases/alignment.py
+++ tests/test_cases/alignment.py
@@ -4,6 +4,14 @@ query = """SELECT abc
 query = """SELECT abc
              FROM xyz
             WHERE def = 'def'"""
+query = """SELECT abc
+             FROM xyz
+            WHERE def = 'def' AND feg = 'feg'"""  # Q445
+query = """SELECT abc
+             FROM xyz
+            WHERE def = 'def'
+              AND feg = 'feg'
+               OR ijk = 'ijk'"""
 query = """INSERT INTO xyz (clm1, clm2)
                 VALUES (abc, def)"""  # Q447
 query = """INSERT INTO xyz (clm1, clm2)
