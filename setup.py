1bac6c78c9a2f7121312a03c276eeea8828ac405
sam-walton <sam.walton@smarkets.com>
2022-06-14 16:32:35 +0100

    Test on Python 3.9/3.10

--- setup.py
+++ setup.py
@@ -38,6 +38,8 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Software Development :: Quality Assurance',
